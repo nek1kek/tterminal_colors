1e2f7ae90ff50422078e4f748a5a40a467f8a986
nek1kek <65557198+nek1kek@users.noreply.github.com>
2023-06-11 17:55:33 +0300

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,33 @@
+class Colors:
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    INVERTED = '\033[7m'
+    CROSSED = '\033[9m'
+
+    # colors
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    CREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    SKYBLUE = '\033[36m'
+    GREY = '\033[37m'
+
+    # background_colors
+    BG_BLACK = '\033[40m'
+    BG_RED = '\033[41m'
+    BG_CREEN = '\033[42m'
+    BG_YELLOW = '\033[43m'
+    BG_BLUE = '\033[44m'
+    BG_PURPLE = '\033[45m'
+    BG_SKYBLUE = '\033[46m'
+    BG_GREY = '\033[47m'
+
+    FAIL = '\033[91m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    OKBLUE = '\033[94m'
+    HEADER = '\033[95m'
+    OKCYAN = '\033[96m'
